78234e372a2cbacee077a45543a715343217e8d6
Greg Sutcliffe <gsutclif@redhat.com>
2012-10-08 17:13:42 +0100

    Pass in variables from jenkins to uploader

--- debian/buildscripts/reprepro-upload.sh
+++ debian/buildscripts/reprepro-upload.sh
@@ -3,7 +3,10 @@
 # Script to sign and upload finished debs to reprepro
 # Assumes root perms (use sudo) and that the deb/changes files are in .
 
+COMPONENT="$1"
+DISTRO="$2"
+
 export GNUPGHOME=/root/test-reprepro/.gnupg
 dpkg-sig -k E775FF07 --sign builder *changes
 
-reprepro -b /root/test-reprepro -C $reponame includedeb $osnameversion *deb
+reprepro -b /root/test-reprepro -C $COMPONENT includedeb $DISTRO *deb
