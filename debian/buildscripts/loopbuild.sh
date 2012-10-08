9d92e0e90a13760cca19dfc02bd2315db2f6e2e9
Greg Sutcliffe <gsutclif@redhat.com>
2012-10-08 15:47:56 +0100

    add sudo to loopbuild for jenkins

--- debian/buildscripts/loopbuild.sh
+++ debian/buildscripts/loopbuild.sh
@@ -3,15 +3,15 @@
 # Sample script to loop over a set of package builds
 
 # Debian 6
-./buildpackage.sh squeeze64 debian/debian6/stable/foreman-proxy
-./buildpackage.sh squeeze32 debian/debian6/stable/foreman-proxy
+sudo ./buildpackage.sh squeeze64 debian/debian6/stable/foreman-proxy
+sudo ./buildpackage.sh squeeze32 debian/debian6/stable/foreman-proxy
 
-./buildpackage.sh squeeze64 debian/debian6/nightly/foreman-proxy
-./buildpackage.sh squeeze32 debian/debian6/nightly/foreman-proxy
+sudo ./buildpackage.sh squeeze64 debian/debian6/nightly/foreman-proxy
+sudo ./buildpackage.sh squeeze32 debian/debian6/nightly/foreman-proxy
 
 # Ubuntu 12.04 LTS
-./buildpackage.sh precise64 debian/ubuntu1204/stable/foreman-proxy
-./buildpackage.sh precise32 debian/ubuntu1204/stable/foreman-proxy
+sudo ./buildpackage.sh precise64 debian/ubuntu1204/stable/foreman-proxy
+sudo ./buildpackage.sh precise32 debian/ubuntu1204/stable/foreman-proxy
 
-./buildpackage.sh precise64 debian/ubuntu1204/nightly/foreman-proxy
-./buildpackage.sh precise32 debian/ubuntu1204/nightly/foreman-proxy
+sudo ./buildpackage.sh precise64 debian/ubuntu1204/nightly/foreman-proxy
+sudo ./buildpackage.sh precise32 debian/ubuntu1204/nightly/foreman-proxy
