2eed1366239ee415298980afc528906155f1eb7d
Greg Sutcliffe <gsutclif@redhat.com>
2012-10-09 18:19:14 +0100

    Update paths to more sensible values, push to deb.theforeman.org/devel

--- debian/buildscripts/reprepro-upload.sh
+++ debian/buildscripts/reprepro-upload.sh
@@ -9,9 +9,13 @@ DEB_PATH="$3"
 
 cd $3
 
-export GNUPGHOME=/root/test-reprepro/.gnupg
+export GNUPGHOME=/root/foreman-reprepro/.gnupg
 dpkg-sig -k E775FF07 --sign builder *changes
 
 # TODO: fix this - can't rebuild packages at the moment (without manual intervention)
-# reprepro -b /root/test-reprepro -C stable remove squeeze foreman-proxy
-reprepro -b /root/test-reprepro -C $COMPONENT includedeb $DISTRO *deb
+# reprepro -b /root/foreman-reprepro -C stable remove squeeze foreman-proxy
+reprepro -b /root/foreman-reprepro -C $COMPONENT includedeb $DISTRO *deb
+
+# Push to shell.theforeman.org
+# Relies on ssh keys being set up
+rsync -av /root/foreman-reprepro/ greg@shell.theforeman.org:deb.theforeman.org/html/devel
