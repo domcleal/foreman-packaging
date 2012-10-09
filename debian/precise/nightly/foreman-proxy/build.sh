15915a6e8b09300bf3b0943ab92daa3ca2e67b73
Greg Sutcliffe <gsutclif@redhat.com>
2012-10-09 15:29:38 +0100

    Add updated precise scripts

--- debian/precise/nightly/foreman-proxy/build.sh
+++ debian/precise/nightly/foreman-proxy/build.sh
@@ -12,28 +12,23 @@
 set -e
 
 PACKAGE_NAME='foreman-proxy'
-VERSION='1.1'
-MAINTAINER='Greg Sutcliffe <greg.sutcliffe@gmail.com>'
 
+# Name of the pbuilder env to use
 PBUILDER="$1"
-BUILD_DIR="$2"
+
+# We use readlink to get an absolute path to the Jenkins
+# checkout, but readlink expects the path to exist
+mkdir -p "../$1"
+BUILD_DIR=`readlink -f ../$1`
 TARGET="${BUILD_DIR}/${PACKAGE_NAME}"
-DEB_STORE="/tmp/debs"
 
 REPO='git://github.com/theforeman/smart-proxy.git'
 BRANCH='develop'
 
-# TODO: For reprepro
-# REPO_DIR='/home/greg/build-area/foreman-repo'
-# DEB_REPO='stable'
-
-DATE=$(date -R)
-UNIXTIME=$(date +%s)
-RELEASE="${VERSION}"
 GIT='/usr/bin/git'
 
 # Copy in packaging to the build dir
-mkdir "${BUILD_DIR}/debian"
+mkdir -p  "${BUILD_DIR}/debian"
 cp -r ./* "${BUILD_DIR}/debian/"
 
 # Clone source code
@@ -43,6 +38,7 @@ cd "${TARGET}"
 $GIT checkout "${BRANCH}"
 $GIT submodule init
 $GIT submodule update
+# Move packaging into source clone
 mv "${BUILD_DIR}/debian" "${TARGET}/"
 
 # Cleanup source
@@ -50,6 +46,11 @@ LAST_COMMIT=$($GIT rev-list HEAD|/usr/bin/head -n 1)
 rm -rf $(/usr/bin/find "${TARGET}" -name '.git*')
 
 # Add 'nightly' to changelog
+
+DATE=$(date -R)
+UNIXTIME=$(date +%s)
+RELEASE="9999-~nightlybuild${UNIXTIME}"
+
 mv debian/changelog debian/changelog.tmp
 
 echo "$PACKAGE_NAME ($RELEASE) UNRELEASED; urgency=low
@@ -64,10 +65,5 @@ cat debian/changelog.tmp >> debian/changelog
 rm -f debian/changelog.tmp
 
 # Execute build using the pbuilder image in $1
-pdebuild-$PBUILDER
-
-# Copy packages
-rm -rf   "${DEB_STORE}/$PBUILDER/nightly/"
-mkdir -p "${DEB_STORE}/$PBUILDER/nightly/"
-cp ../*changes "${DEB_STORE}/$PBUILDER/nightly/"
-cp ../*deb     "${DEB_STORE}/$PBUILDER/nightly/"
+sudo pdebuild-$PBUILDER
+sudo chown -R jenkins:jenkins "${BUILD_DIR}"
